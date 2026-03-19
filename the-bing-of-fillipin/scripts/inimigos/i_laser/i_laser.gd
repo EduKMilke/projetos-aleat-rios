f3197889b9e8b366049c3ab0f31e353afd74051c
Eduardo K. Milke <edukmilke@gmail.com>
2026-03-19 19:14:45 -0300

    inimigo que solta laser pronto]

--- the-bing-of-fillipin/scripts/inimigos/i_laser/i_laser.gd
+++ the-bing-of-fillipin/scripts/inimigos/i_laser/i_laser.gd
@@ -1,20 +1,36 @@
 extends StaticBody2D
 
-var player=null
-var laser=preload("res://obj/inimigos/laser.tscn")
-var a=true
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var player = null
+var laser_scene = preload("res://obj/inimigos/laser.tscn")
+var pode_atirar = true
+var vida=10
+@onready var ani = $AnimatedSprite2D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if player == null:
 		player = get_tree().get_first_node_in_group("player")
+	if vida<=0:
+		queue_free()
 	if player != null and global_position.distance_to(player.global_position) < 800:
-		if a==true:
-			await get_tree().create_timer(1).timeout
-			var i_laser=laser.instantiate()
-			add_child(i_laser)
-			a=false
+		if pode_atirar:
+			disparar_sequencia()
+
+func disparar_sequencia() -> void:
+	pode_atirar = false
+	
+	ani.frame = 1 
+	await get_tree().create_timer(0.5).timeout
+	
+	ani.frame = 2
+	await get_tree().create_timer(0.5).timeout
+	
+	var novo_laser = laser_scene.instantiate()
+	var posicao_alvo = player.global_position
+	await get_tree().create_timer(0.2).timeout
+	add_child(novo_laser)
+	
+	if novo_laser.has_method("configurar_alvo"):
+		novo_laser.configurar_alvo(posicao_alvo)
+	
+	await get_tree().create_timer(1.0).timeout
+	pode_atirar = true
--- the-bing-of-fillipin/scripts/inimigos/i_laser/laser.gd
+++ the-bing-of-fillipin/scripts/inimigos/i_laser/laser.gd
@@ -3,34 +3,52 @@ extends RayCast2D
 @export var color := Color.CYAN: set = set_color
 @export var beam_width := 4.0
 @export var max_length := 2000.0
-var player= null
 
-# Tente usar o $Line2D se o % não estiver funcionando
 @onready var line_2d: Line2D = get_node_or_null("Line2D")
+@onready var collision_shape: CollisionShape2D = $Area2D/CollisionShape2D
 
 func _ready() -> void:
-	if line_2d == null:
-		push_error("ERRO: O nó Line2D não foi encontrado! Verifique o nome na árvore de cena.")
-		return
-		
+	if line_2d == null: return
+	
 	line_2d.points = [Vector2.ZERO, Vector2.ZERO]
 	line_2d.width = beam_width
 	set_color(color)
+	
 	target_position = Vector2(max_length, 0)
-	await get_tree().create_timer(1).timeout
+	
+	await get_tree().create_timer(1.0).timeout
 	queue_free()
+
+func configurar_alvo(pos_alvo: Vector2) -> void:
+	look_at(pos_alvo)
+
 func _physics_process(_delta: float) -> void:
 	if line_2d == null: return
-	if player == null:
-		player = get_tree().get_first_node_in_group("player")
-	look_at(player.global_position)
-	var cast_point := target_position
+	
 	force_raycast_update()
+	
+	var cast_point := target_position
+	
 	if is_colliding():
 		cast_point = to_local(get_collision_point())
+	
 	line_2d.set_point_position(1, cast_point)
+	atualizar_colisao_area(cast_point)
+
+func atualizar_colisao_area(ponto_final: Vector2) -> void:
+	if collision_shape.shape is SegmentShape2D:
+		collision_shape.shape.a = Vector2.ZERO
+		collision_shape.shape.b = ponto_final
+	elif collision_shape.shape is RectangleShape2D:
+		var length = ponto_final.length()
+		collision_shape.shape.size = Vector2(length, beam_width)
+		collision_shape.position = Vector2(length / 2, 0)
 
 func set_color(new_color: Color) -> void:
 	color = new_color
 	if line_2d:
 		line_2d.default_color = new_color
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		Global.menos_vida()
